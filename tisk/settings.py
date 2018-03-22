ec04630d7c3e78ac3e53dbddcf861b95100cc77e
Evans Munene <munene.evansk@gmail.com>
2018-03-23 00:38:35 +0300

    [worked out migration]

--- tisk/settings.py
+++ tisk/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'tisk.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tisk',
+        'USER': 'tisk_user',
+        'PASSWORD': 'tisk_password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -102,6 +106,17 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+}
 
 
 # Internationalization
