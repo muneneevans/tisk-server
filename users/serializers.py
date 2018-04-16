cfc2e1338bb60f5f8b666e760bfc8974950794f0
Evans Munene <munene.evansk@gmail.com>
2018-04-16 23:17:40 +0300

    [FIX] phone number issue

--- users/serializers.py
+++ users/serializers.py
@@ -15,7 +15,7 @@ class UserCreateSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('email', 'password', 'Phone_number', 'first_name', 'last_name', 'national_id')
+        fields = ('email', 'password', 'phone_number', 'first_name', 'last_name', 'national_id')
 
     def create(self, validated_data):
         created_user = User.objects.create_user(**validated_data)
