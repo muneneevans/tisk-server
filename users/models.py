bef7c5e6ff60e5aba4c4a2e5d1499f5b9ff983e6
Evans Munene <munene.evansk@gmail.com>
2018-04-16 23:20:15 +0300

    [FIX] phone number issue

--- users/models.py
+++ users/models.py
@@ -14,7 +14,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     first_name = models.CharField(_('first name'), max_length=30, blank=True)
     last_name = models.CharField(_('last name'), max_length=30, blank=True)
     gender = models.CharField(_('gender'), max_length=30, blank=True)
-    phone_number = models.CharField(_('phone_number'), max_length=30, blank=True)
+    phone_number = models.CharField(_('phone_number'), max_length=30, unique=True )
     date_joined = models.DateTimeField(_('date joined'), auto_now_add=True)
     date_modified = models.DateTimeField(('date modified'), auto_now=True)
     national_id = models.CharField(_('national_id'), max_length=200, unique=True)
