4488622f29a1f1a3f67816a8f028a6c457cbc239
Brian Keyonzo <briankeyonzo@yahoo.com>
2018-05-02 12:25:50 +0300

    [ADD] Added two new endpoints request_mfs and activate_mfs, fixed member_types api url, made some endpoints more browsable

--- users/api.py
+++ users/api.py
@@ -1,12 +1,13 @@
-from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.fields import EmailField, CharField
-from rest_framework.generics import ListCreateAPIView, ListAPIView, RetrieveAPIView, GenericAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveAPIView, GenericAPIView
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 from rest_framework.serializers import Serializer
+from rest_framework_jwt.compat import PasswordField
 
+from members.permissions import IsMFSInactive
 from .permissions import isOwner
 from .serializers import *
 
@@ -49,9 +50,9 @@ class ActivateUser(GenericAPIView):
             user.is_active = True
             user.save()
 
-            return JsonResponse({"message": "user activation successful"})
+            return Response({"message": "user activation successful"})
         else:
-            return JsonResponse(
+            return Response(
                 {
                     'status': 'invalid token',
                     'message': "expired token"
@@ -71,10 +72,49 @@ class ResendActivationEmail(GenericAPIView):
 
         if not user.is_active:
             user.send_activation_email()
-            return JsonResponse({"message": "Activation email successfully sent"})
+            return Response({"message": "Activation email successfully sent"})
         else:
-            return JsonResponse(
+            return Response(
                 {
                     'status': 'Invalid operation',
                     'message': "user is already activated"
                 }, status=403)
+
+
+class RequestMFS(GenericAPIView):
+    permission_classes = [IsMFSInactive]
+    serializer_class = type('', (Serializer,), {'password': PasswordField(required=True, max_length=50, allow_blank=False, allow_null=False)})
+
+    def post(self, request, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        if not request.user.check_password(serializer.validated_data['password']):
+            return Response({
+                    'status': 'Invalid operation',
+                    'message': "Invalid password"
+                }, status=400)
+
+        member = request.user.member
+        # TODO: Add code to register MFS account
+
+        return Response({'status': 'success', 'message': 'Successfully created MFS account'}, status=200)
+
+
+class ActivateMFS(GenericAPIView):
+    permission_classes = [IsMFSInactive]
+    serializer_class = type('', (Serializer,), {'token': CharField(required=True, max_length=50, allow_blank=False, allow_null=False)})
+
+    def post(self, request, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        token = serializer.validated_data['token']
+
+        # TODO: Send activation token to MFS, activate user locally and populate relevant fields and relay response
+        # to user
+
+        return Response({'status': 'success', 'message': 'Successfully activated MFS account'}, status=200)
+
+
+
--- users/urls.py
+++ users/urls.py
@@ -10,10 +10,11 @@ api_urls = [
     url(r'membershiptypes', include("member_types.urls")),
     url(r'new/?', CreateUser.as_view(), name='new-user'),
     url(r'activate/$', ActivateUser.as_view(), name='activate-user'),
-    url(r'(?P<email>([\w+.]+)\@([\w+.]+))/$',
-        RetrieveUserView.as_view(), name='user-view'),
+    url(r'(?P<email>([\w+.]+)\@([\w+.]+))/$', RetrieveUserView.as_view(), name='user-view'),
     url(r'(?P<email>([\w+.]+)\@([\w+.]+))/savings', include("savings.urls")),
     url(r'send_activation_email/', ResendActivationEmail.as_view()),
+    url(r'request_mfs/', RequestMFS.as_view()),
+    url(r'activate_mfs/', ActivateMFS.as_view()),
 
 
 ]
