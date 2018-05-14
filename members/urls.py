64eecebd651976c6f86814ae5e2bb0eea19429f6
Brian Keyonzo <briankeyonzo@yahoo.com>
2018-05-14 21:37:03 +0300

    [UPDATE] Moved MFS endpoints to members module

--- members/urls.py
+++ members/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url, include
-from members.api import ActivateMFS
+from members.api import ActivateMFS, RequestMFS
+
 app_name = 'members'
 
 
 api_urls = [
-    url(r'activate_mfs/?', ActivateMFS.as_view()),
+    url(r'request_mfs/', RequestMFS.as_view()),
+    url(r'activate_mfs/', ActivateMFS.as_view()),
 ]
