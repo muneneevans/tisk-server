8b1f1003f34e632c7c2cd6b6f9ac9808bd765a0f
muneneevans <munene.evansk@gmail.com>
2018-05-16 11:47:34 +0300

    [HOPE]

--- members/urls.py
+++ members/urls.py
@@ -7,5 +7,10 @@ app_name = 'members'
 api_urls = [
     url(r'request_mfs/', RequestMFS.as_view()),
     url(r'activate_mfs/', ActivateMFS.as_view()),
+    # url(r'^$',  UserMembershipView.as_view(), name='user-membership-view'),
+]
+
+urlpatterns = [
+
     url(r'^$',  UserMembershipView.as_view(), name='user-membership-view'),
 ]
