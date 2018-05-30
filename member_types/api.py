c6d4b20ce09cd19cc550b3131f3135e6f075284f
Brian Keyonzo <briankeyonzo@yahoo.com>
2018-05-30 11:52:47 +0300

    [ADD] Added MemberTypeViewSet

--- member_types/api.py
+++ member_types/api.py
@@ -1,10 +1,13 @@
+from rest_framework import mixins
 from rest_framework.generics import ListAPIView
 from rest_framework.permissions import AllowAny, IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
+
 from .serializers import *
 from .models import *
 
 
-class MemberTypesListView(ListAPIView): 
+class MemberTypeViewSet(mixins.RetrieveModelMixin,mixins.ListModelMixin,GenericViewSet):
     model = MemberType    
     queryset = MemberType.objects.all()
     serializer_class = MemberTypeInlineSerializer
--- member_types/urls.py
+++ member_types/urls.py
@@ -1,14 +1,18 @@
 from django.conf.urls import url, include
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 
 from .api import *
-
+from .api import MemberTypeViewSet
 
 app_name= "member_types"
 
+router = DefaultRouter()
+router.register(r'', MemberTypeViewSet)
+
 
 api_urls = [
-    path(r'',  MemberTypesListView.as_view(), name='member-types-list-view'),
+    path(r'',  include(router.urls))
 ]
 
 
